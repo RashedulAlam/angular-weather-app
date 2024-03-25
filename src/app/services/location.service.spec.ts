8998921fd7c1224eeaff5836a8f59cbe13b8abbb
Rashedul-Alam-Rudra <rashedulalam.iit.du@gmail.com>
2024-03-26 00:20:03 +0200

    added unit tests

--- src/app/services/location.service.spec.ts
+++ src/app/services/location.service.spec.ts
@@ -1,16 +1,66 @@
+import {
+  HttpClientTestingModule,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
 import { LocationService } from './LocationService';
+import { of, skip } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 describe('LocationService', () => {
   let service: LocationService;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const spy = jasmine.createSpyObj('HttpClient', ['get']);
+
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [LocationService, { provide: HttpClient, useValue: spy }],
+    });
     service = TestBed.inject(LocationService);
+    httpClientSpy = TestBed.inject(HttpClient) as jasmine.SpyObj<HttpClient>;
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get user location', () => {
+    const mockPosition = {
+      coords: {
+        latitude: 123,
+        longitude: 456,
+      },
+    } as any;
+
+    spyOn(navigator.geolocation, 'getCurrentPosition').and.callFake(
+      (success) => {
+        success(mockPosition);
+      }
+    );
+
+    service
+      .getLocation()
+      .pipe(skip(1))
+      .subscribe((location) => {
+        expect(location).toEqual({ lat: 123, lng: 456 });
+      });
+  });
+
+  it('should get location name', () => {
+    spyOn(service, 'getLocation').and.returnValue(of({ lat: 1.1, lng: 2.2 }));
+
+    const mockLocationResponse = {
+      results: [
+        {
+          formatted_address: 'Mock Location',
+        },
+      ],
+    };
+    httpClientSpy.get.and.returnValue(of(mockLocationResponse));
+
+    service.getLocationName().subscribe((locationName) => {
+      expect(locationName).toBe('Mock Location');
+    });
   });
 });
--- src/app/services/weather.service.spec.ts
+++ src/app/services/weather.service.spec.ts
@@ -1,16 +1,44 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
 import { WeatherService } from './weather.service';
+import { environment } from '../../environments/environment';
 
 describe('WeatherService', () => {
   let service: WeatherService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [WeatherService],
+    });
     service = TestBed.inject(WeatherService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should fetch current weather statistics', () => {
+    const mockLat = 123;
+    const mockLong = 456;
+
+    const mockWeatherData = {
+    };
+
+    service.getCurrentStat(mockLat, mockLong).subscribe((weatherData) => {
+      expect(weatherData).toBeTruthy();
+    });
+
+    const req = httpMock.expectOne(
+      `${environment.weatherAPIHostUrl}v1/forecast?latitude=${mockLat}&longitude=${mockLong}&current=temperature_2m,precipitation,rain,cloud_cover,wind_speed_10m,relative_humidity_2m,apparent_temperature,snowfall&daily=sunrise&daily=sunset&daily=temperature_2m_max&daily=temperature_2m_min&timezone=${Intl.DateTimeFormat().resolvedOptions().timeZone}&forecast_days=6&hourly=temperature_2m,precipitation,rain,cloud_cover,wind_speed_10m,relative_humidity_2m,apparent_temperature,snowfall`
+    );
+    expect(req.request.method).toBe('GET');
+    req.flush(mockWeatherData);
   });
 });
